b5459ab78e7fa93bc072f418eacd49fb88a0c186
Colin Lassey <colinlassey@mac.mynetworksettings.com>
2024-12-17 20:01:18 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import path from "path";
+import router from './routes/routes.js';
+import url from "url";
+
+const PORT = process.env.PORT;
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(express.json());
+app.use('/api', router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
